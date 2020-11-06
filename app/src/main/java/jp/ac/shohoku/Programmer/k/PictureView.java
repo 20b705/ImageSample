01ed0c28891dd7e6e6ffbbfa698d017670b8216d
kazuhiro kakinuma <s20b705@shohoku.ac.jp>
2020-11-06 11:32:24 +0900

    activity pictuterとpictuerViewの編集

---
+++ app/src/main/java/jp/ac/shohoku/Programmer/k/PictureView.java
@@ -0,0 +1,21 @@
+package jp.ac.shohoku.Programmer.k;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PictureView extends View {
+    public PictureView(Context context) {
+        super(context);
+    }
+
+    public PictureView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PictureView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
